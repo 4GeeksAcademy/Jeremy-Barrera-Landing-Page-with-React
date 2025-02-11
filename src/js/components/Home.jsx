7f2de0af77f7b14c81833a161fb9d927d5d174af
JeremyBarrera <112196500+JeremyBarrera@users.noreply.github.com>
2025-02-11 01:04:33 +0000

    Created seperate files for Navbar & Jumbotron

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import Card from "./Card.jsx"
+import Jumbotron from './Jumbotron.jsx';
 //create your first component
 const Home = () => {
 	return (
+
 		<div class="container-fluid" style={{ maxWidth: "80rem" }}>
-			<div class="jumbotron jumbotron-fluid bg-light p-4" >
-				<div class="container">
-					<h1 class="display-4">A Warm Welcome</h1>
-					<p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam suscipit similique quaerat tempora excepturi laborum doloremque nihil quia possimus vitae, doloribus temporibus quos, est maiores hic a nobis sunt nesciunt!</p>
-				</div>
-				<a href="#" class="btn btn-primary my-3">
-                    Call to action!
-                </a>
-			</div>
+			<Jumbotron/>
 			<div class="row justify-content-center">
 				<div class="col-md-3 col-sm-6 col-12 mb-4"><Card /></div>
 				<div class="col-md-3 col-sm-6 col-12 mb-4"><Card /></div>
@@ -20,7 +14,7 @@ const Home = () => {
 				<div class="col-md-3 col-sm-6 col-12 mb-4"><Card /></div>
 			</div>
 		</div>
-		
+	
 	);
 };
 
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Jumbotron = () => {
+	return (
+        <div class="jumbotron jumbotron-fluid bg-light p-4" >
+            <div class="container">
+                <h1 class="display-4">A Warm Welcome</h1>
+                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam suscipit similique quaerat tempora excepturi laborum doloremque nihil quia possimus vitae, doloribus temporibus quos, est maiores hic a nobis sunt nesciunt!</p>
+            </div>
+            <a href="#" class="btn btn-primary my-3">
+                Call to action!
+            </a>
+        </div>
+    );
+};
+
+export default Jumbotron;
